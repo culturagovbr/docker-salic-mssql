ecf6f04b55a228047987908138957a005c14ae7c
Cleber Santos <oclebersantos@gmail.com>
2019-05-13 16:07:48 -0300

    Corrige nome da variavel de ambiente para senha

--- docker-entrypoint-initdb.sh
+++ docker-entrypoint-initdb.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 # wait for database to start...
+echo "$0: inicializando dump da estrutura"
+
 for i in {30..0}; do
-  if sqlcmd -U SA -P $MSSQL_SA_PASSWORD -Q 'SELECT 1;' &> /dev/null; then
+  if sqlcmd -U SA -P $SA_PASSWORD -Q 'SELECT 1;' &> /dev/null; then
     echo "$0: SQL Server started"
     break
   fi
@@ -16,8 +18,8 @@ do
   echo importando $entry
   shortname=$(echo $entry | cut -f 1 -d '.' | cut -f 2 -d '/')
   echo executing $shortname
-  /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P "salic@123456" -Q 'RESTORE FILELISTONLY FROM DISK = "/tmp/schemas/'$shortname'.bak"' | tr -s ' ' | cut -d ' ' -f 1-2
-  /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P "salic@123456" -Q 'RESTORE DATABASE '$shortname' FROM DISK = "/tmp/schemas/'$shortname'.bak" WITH MOVE "'$shortname'" TO "/var/opt/mssql/data/'$shortname'.mdf", MOVE "'$shortname'_log" TO "/var/opt/mssql/data/'$shortname'_log.ldf"'
+  /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P $SA_PASSWORD -Q 'RESTORE FILELISTONLY FROM DISK = "/tmp/schemas/'$shortname'.bak"' | tr -s ' ' | cut -d ' ' -f 1-2
+  /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P $SA_PASSWORD -Q 'RESTORE DATABASE '$shortname' FROM DISK = "/tmp/schemas/'$shortname'.bak" WITH MOVE "'$shortname'" TO "/var/opt/mssql/data/'$shortname'.mdf", MOVE "'$shortname'_log" TO "/var/opt/mssql/data/'$shortname'_log.ldf"'
 done
 
 # echo "$0: inicializando dump da estrutura"
@@ -32,10 +34,10 @@ done
 
 echo "$0: inicializando dump da base"
 for f in docker-entrypoint-initdb.d/*; do
-  echo "teste $f";
+  echo "carregando $f";
   case "$f" in
     *.sh)     echo "$0: running $f"; . "$f" ;;
-    *.sql)    echo "$0: running $f"; /opt/mssql-tools/bin/sqlcmd -U SA -P "salic@123456" -X -i  "$f"; echo ;;
+    *.sql)    echo "$0: running $f"; /opt/mssql-tools/bin/sqlcmd -U SA -P $SA_PASSWORD -X -i  "$f"; echo ;;
     *)        echo "$0: ignoring $f" ;;
   esac
   echo
