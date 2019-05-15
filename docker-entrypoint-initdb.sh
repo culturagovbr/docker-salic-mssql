e1fa599ffafe61e7468b8c4024608b034acd0697
Cleber Santos <oclebersantos@gmail.com>
2019-05-15 16:14:00 -0300

    [update] criando versao minima
    Co-authored-by: Paulo Henrique <paulotux.mendes@gmail.com>
    Co-authored-by: Vinicius Feitosa <viniciusfesil@gmail.com>

--- docker-entrypoint-initdb.sh
+++ docker-entrypoint-initdb.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
 # wait for database to start...
-echo "$0: inicializando dump da estrutura"
 
-for i in {30..0}; do
-  if sqlcmd -U SA -P $SA_PASSWORD -Q 'SELECT 1;' &> /dev/null; then
-    echo "$0: SQL Server started"
-    break
-  fi
-  echo "$0: SQL Server startup in progress..."
-  sleep 1
-done
+if [ ! -z "$SA_PASSWORD" ]; then
+  echo "$0: inicializando dump da estrutura"
+  for i in {30..0}; do
+    if sqlcmd -U SA -P $SA_PASSWORD -Q 'SELECT 1;' &> /dev/null; then
+      echo "$0: SQL Server started"
+      break
+    fi
+    echo "$0: SQL Server startup in progress..."
+    sleep 1
+  done
+fi
 
 if [ "$GERAR_ESTRUTURA_E_DADOS" == "true" ]; then
 	echo "[ ****************** ] Inicializando dump."
